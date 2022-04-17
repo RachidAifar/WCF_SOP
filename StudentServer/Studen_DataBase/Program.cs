c480f0427a4e1d94c9b8bbb1c72de81d48347fdd
Rachid Aifar <90982692+RachidAifar@users.noreply.github.com>
2022-04-17 15:38:49 +0200

    Add files via upload

---
+++ StudentServer/Studen_DataBase/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studen_DataBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
